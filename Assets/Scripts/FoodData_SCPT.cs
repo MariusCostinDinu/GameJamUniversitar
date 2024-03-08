bd79e0e4766a6113a54417aa437fe8c85e1a8690
Denis-Viorel <61794142+Denis-Viorel@users.noreply.github.com>
2024-03-08 10:23:13 +0200

    HealthComponent

---
+++ Assets/Scripts/FoodData_SCPT.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodData_SCPT : MonoBehaviour
+{
+    public struct FoodData
+    {
+        public string name;
+        public string type;
+        public int hunger;
+        /* To be implemented
+        public despawner; */
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
